e9ceca6fa62deca765c0e79432e8ebe1e8953bc8
Simon L <31144712+slxfld@users.noreply.github.com>
2018-12-30 00:41:36 +0100

    Update Networking.java

--- MiniGolf_Projekt/src/Main_Spiel/Networking.java
+++ MiniGolf_Projekt/src/Main_Spiel/Networking.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Main_Spiel;
 
 import java.io.ObjectInputStream;
@@ -12,10 +8,6 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- *
- * @author SLZX
- */
 public class Networking extends Main {
    
     
